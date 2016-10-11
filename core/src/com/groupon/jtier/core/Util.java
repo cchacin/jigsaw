703af2bb8042571cd5cf2eba54c3a0b25fa7c4cc
Carlos Chacin <cchacin@gmail.com>
2016-10-10 18:27:40 -0700

    Simple ant project

---
+++ core/src/com/groupon/jtier/core/Util.java
@@ -0,0 +1,6 @@
+package com.groupon.jtier.core;
+
+import com.groupon.jtier.core.internal.InternalUtility;
+
+public class Util extends InternalUtility {
+}
---
+++ core/src/com/groupon/jtier/core/internal/InternalUtility.java
@@ -0,0 +1,7 @@
+package com.groupon.jtier.core.internal;
+
+public class InternalUtility {
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
